199f3c117e1aabca3eafa0aaad9dbcf3b023c331
Lucas Redigolo <lucas_redigolo@hotmail.com>
2018-01-17 21:49:31 -0200

    FIM DO HTTP POST

--- WebServicesCidades/Controllers/PrimeiraController.cs
+++ WebServicesCidades/Controllers/PrimeiraController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WebServicesCidades.Models;
 
@@ -7,10 +8,22 @@ namespace WebServicesCidades.Controllers {
     [Route ("api/[controller]")]
     public class PrimeiraController : Controller {
         Cidades cidade = new Cidades ();
+        DAOCidades dao = new DAOCidades();
 
         [HttpGet]
         public IEnumerable<Cidades> Get () {
-            return cidade.Listar();
+            return dao.Listar();
+        }
+
+        [HttpGet("{ID}", Name = "CidadeAtual")]
+        public Cidades Get(int id){
+            return dao.Listar().Where(x => x.ID==id).FirstOrDefault();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Cidades cidades){
+            dao.Cadastro(cidades);
+            return CreatedAtRoute("CidadeAtual", new {id=cidades.ID},cidades);
         }
     }
 }
--- WebServicesCidades/Models/Cidades.cs
+++ WebServicesCidades/Models/Cidades.cs
@@ -6,14 +6,5 @@ namespace WebServicesCidades.Models {
         public string Nome { get; set; }
         public string Estado { get; set; }
         public int Habitantes { get; set; }
-
-        public List<Cidades> Listar () {
-            return new List<Cidades> () {
-                new Cidades { ID = 10, Nome = "Leme", Estado = "SP", Habitantes = 312 },
-                new Cidades { ID = 32, Nome = "Curitiba", Estado = "PR", Habitantes = 565 },
-                new Cidades { ID = 65, Nome = "Florianopolis", Estado = "SC", Habitantes = 45 },
-                new Cidades { ID = 75, Nome = "Rio de Janeiro", Estado = "RJ", Habitantes = 98876 }
-            };
-        }
     }
 }
