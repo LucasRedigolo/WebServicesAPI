20f10699b7bb0c1d6663b0967cb93d08b66b0515
Lucas Redigolo <lucas_redigolo@hotmail.com>
2018-01-19 18:53:06 -0200

    divisão de rotas

--- WebServicesCidades/Controllers/PrimeiraController.cs
+++ WebServicesCidades/Controllers/PrimeiraController.cs
@@ -5,25 +5,27 @@ using WebServicesCidades.Models;
 
 namespace WebServicesCidades.Controllers {
     //Vamos definir a rota para a requisição do serviço
-    [Route ("api/[controller]")]
     public class PrimeiraController : Controller {
         Cidades cidade = new Cidades ();
-        DAOCidades dao = new DAOCidades();
+        DAOCidades dao = new DAOCidades ();
 
         [HttpGet]
+        [Route ("api/GetAll")]
         public IEnumerable<Cidades> Get () {
-            return dao.Listar();
+            return dao.Listar ();
         }
 
-        [HttpGet("{ID}", Name = "CidadeAtual")]
-        public Cidades Get(int id){
-            return dao.Listar().Where(x => x.ID==id).FirstOrDefault();
+        [HttpGet ("{ID}", Name = "CidadeAtual")]
+        [Route ("api/GetAll/{ID}")]
+        public Cidades Get (int id) {
+            return dao.Listar ().Where (x => x.ID == id).FirstOrDefault ();
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody] Cidades cidades){
-            dao.Cadastro(cidades);
-            return CreatedAtRoute("CidadeAtual", new {id=cidades.ID},cidades);
+        [Route ("api/AddCity")]
+        public IActionResult Post ([FromBody] Cidades cidades) {
+            dao.Cadastro (cidades);
+            return CreatedAtRoute ("CidadeAtual", new { id = cidades.ID }, cidades);
         }
     }
 }
